e1ae6140f4fece9c10d1195fc1e6fe48eec1aa70
Sergio Schmilovich <sergio.schmilovich@gmail.com>
2022-08-24 16:12:37 -0500

    update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 mkdir -p logs
 cd /home/sergio/scripts/calendar_icons/
+find logs -type f -mtime +30 -delete
 . ./env/bin/activate
 /home/sergio/scripts/calendar_icons/env/bin/python3 /home/sergio/scripts/calendar_icons/main.py >> logs/calendar_icons-"`date +"%Y-%m-%d_%H.%M.%S"`".log 2>&1
 deactivate
